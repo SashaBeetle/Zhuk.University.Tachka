113056b064441c38c1a45b8dbfda8123257ef59e
Олександр Жук <oleksandr.zhuk@lnu.edu.ua>
2023-04-19 17:54:46 +0300

    fixed Bootstrap

---
+++ Zhuk.University.Tachka.Web/Controllers/CarsApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zhuk.University.Tachka.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsApiController : ControllerBase
+    {
+    }
+}
--- Zhuk.University.Tachka.Web/Pages/Index.cshtml.cs
+++ Zhuk.University.Tachka.Web/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Zhuk.University.Tachka.Web.Pages
@@ -6,15 +8,15 @@ namespace Zhuk.University.Tachka.Web.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+
         }
 
         public void OnGet()
         {
-
+          
         }
     }
 }
