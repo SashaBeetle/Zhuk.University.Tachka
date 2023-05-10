5f42cf8b76b8e3df957b52ef079053659314e07b
Олександр Жук <oleksandr.zhuk@lnu.edu.ua>
2023-05-10 19:28:19 +0300

    Registration Compleate.

--- Zhuk.University.Tachka.Web/Program.cs
+++ Zhuk.University.Tachka.Web/Program.cs
@@ -4,32 +4,45 @@ using Microsoft.Extensions.DependencyInjection;
 using Zhuk.University.Tachka.Web.Data;
 using Zhuk.University.Tachka.Database;
 using Zhuk.University.Tachka.Web;
-
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.RegisterDatabaseDependencies(builder.Configuration);
-
+//builder.Services.RegisterIdentityDependencies();
 builder.Services.AddApplicationInsightsTelemetry();
 
-builder.Services.AddAutoMapper(typeof(UserProfile));
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ZhukUniversityTachkaWebContext>(options =>
     
     options.UseSqlServer(builder.Configuration.GetConnectionString("ZhukUniversityTachkaWebContext") ?? throw new InvalidOperationException("Connection string 'ZhukUniversityTachkaWebContext' not found.")));
 
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<TachkaDbContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/");
+});
 
 var app = builder.Build();
 app.MapControllers();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -37,6 +50,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseRouting();
 
