2c4fa43c1583407d23b4dc6788eabb187157fdf7
Олександр Жук <oleksandr.zhuk@lnu.edu.ua>
2023-06-15 14:33:29 +0300

    Added my API

---
+++ Zhuk.University.Tachka.Web/Controllers/CarController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Zhuk.University.Tachka.Database.Interfaces;
+using Zhuk.University.Tachka.Models.Database;
+
+namespace Zhuk.University.Tachka.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarController : ControllerBase
+    {
+
+       
+            private readonly IDbEntityService<Car> _carService;
+
+            public CarController(IDbEntityService<Car> carService)
+            {
+                _carService = carService;
+            }
+
+            [HttpGet]
+            public async Task<IActionResult> GetAllCars()
+            {
+                List<Car> cars = await _carService.GetAll().ToListAsync();
+
+                return Ok(cars);
+            }
+        
+    }
+}
