4ec669b2fe772d4f663b5e32f8d5c324edafc498
Олександр Жук <oleksandr.zhuk@lnu.edu.ua>
2023-07-03 12:13:03 +0300

    Created Avatar generator.

---
+++ Zhuk.University.Tachka.Database/Interfaces/IAvatarHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zhuk.University.Tachka.Database.Interfaces
+{
+    public interface IAvatarHelper
+    {
+        string GetRandomAvatarUrl(string gender);
+    }
+}
