8b2db5ba8db9ea8843112753f9af7a3d2a52de86
Олександр Жук <oleksandr.zhuk@lnu.edu.ua>
2024-05-07 23:06:15 +0300

    Fix some errors with page.

--- Zhuk.University.Tachka.Web/Pages/User/EditCar.cshtml.cs
+++ Zhuk.University.Tachka.Web/Pages/User/EditCar.cshtml.cs
@@ -28,10 +28,10 @@ namespace Zhuk.University.Tachka.Web.Pages.User
         {
             OwnCar = await _carService.GetById(id);
 
-            if (OwnCar.UserId != User.Identity.Name)
-            {
-                return RedirectToPage("/Error");
-            }
+            //if (OwnCar.UserId != User.Identity.Name)
+            //{
+            //    return RedirectToPage("/Error");
+            //}
 
             Colors = ColorsRep.GetAllColors().ToList();
             Years = YearHelper.GetYearsList().ToList();
